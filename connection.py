786ded7f3838a54f9f01c446daa1bd1f743f17ba
unknown <tech.adel87@gmail.com>
2022-10-22 10:46:59 +0200

    Modified to Context Manager

--- connection.py
+++ connection.py
@@ -1,5 +1,6 @@
 import sqlite3
 import aiosqlite
+import asyncio
 from datetime import timedelta
 from datetime import datetime
 
@@ -9,9 +10,9 @@ time = datetime.now().strftime('%H:%M')
 
 def dict_factory(cursor, row):
     """
-    Convert result to dic
+    Convert cursor to dic
     Key = table descrition
-    Value = the returned value it self
+    Value = row aurgment
     """
     d = {}
     for idx, col in enumerate(cursor.description):
@@ -20,86 +21,58 @@ def dict_factory(cursor, row):
 
 class DataBase:
     def __init__(self, db):
-        self.db = db
-        self.conn = sqlite3.connect(self.db)
+        self._sqlite = sqlite3.connect(db)
+        self._aiosqlite = aiosqlite.connect(db)
 
     def get_table(self, table_name):
         """
         Get table rows in a dictioneries format
-        {'line_id': x, 'line_name': y}
+        column descroption = column value
         """
-        conn = sqlite3.connect(self.db)
-
-        with conn:
+        with self._sqlite as conn:
             conn.row_factory = dict_factory
-            curs = conn.cursor()
-            curs.execute(f"SELECT * FROM {table_name}")
-            rows = curs.fetchall()
-            return rows
+            return conn.execute(f"SELECT * FROM {table_name}").fetchall()
 
     # Create Line Result
     def create_today_row(self, line):
-        """Create empty row  for argumnet line and make sure no duplicate rows is exists"""
-        conn = sqlite3.connect(self.db)
-        # validating the row existing
-        curs = conn.execute(
-                f"""SELECT * FROM results WHERE line_name = '{line['line_name']}' and date = '{today}'""")
-        rows = curs.fetchall()     
-        if rows:
-            for row in rows:
-                conn.execute(f"DELETE from results where id={row[0]}")
-                conn.commit()
-        conn.execute(f"""INSERT INTO results ('line_id', 'line_name', 'isp', 'line_using', 'line_number', 'date', 'time', 'upload', 'download') 
-        VALUES
-        ('{line['line_id']}', '{line['line_name']}', '{line['isp']}', '{line['line_using']}',  {line['line_number']}, '{today}', '{time}', '0', '0')""")
-        conn.commit()
-        conn.close()
+        """
+        Search for today line row
+        Delete it if found
+        Create today empty row
+        """
+        with self._sqlite as conn:  # validating the row existing
+            rows = conn.execute(
+                f"""SELECT * FROM results WHERE line_name = '{line['line_name']}' and date = '{today}'""").fetchall()
+            if rows:
+                for row in rows:
+                    conn.execute(f"DELETE from results where id={row['id']}")
+            conn.execute(f"""INSERT INTO results ('line_id', 'line_name', 'isp', 'line_using', 'line_number', 'date', 'time', 'upload', 'download') 
+            VALUES
+            ('{line['line_id']}', '{line['line_name']}', '{line['isp']}', '{line['line_using']}',  {line['line_number']}, '{today}', '{time}', '0', '0')""")
 
     # Retreive Old data from table by day
-    async def get_old_value(self, line, target, days):
-        day = datetime.strftime(datetime.now() - timedelta(days), '%d-%m-%Y')
-        print(line)
-        db = await aiosqlite.connect(self.db)
-        try:
-            cursor = await db.execute(
-                f"""SELECT {target} FROM results WHERE line_id="{line['line_id']}" AND date='{day}' ORDER BY id ASC LIMIT 1""")
-            row = await cursor.fetchone()
-            await cursor.close()
-            await db.close()
-            return (row[0])
-        except Exception as ex:
-            print
-
+    async def get_old_value(self, line, target):
+        async with self._aiosqlite as db:
+            day = datetime.strftime(datetime.now() - timedelta(1), '%d-%m-%Y')
+            db.row_factory = aiosqlite.Row
+            async with db.execute(f"""SELECT {target} FROM results WHERE line_name='{line}' AND date='{day}' ORDER BY id ASC LIMIT 1""") as cursor:
+                async for row in cursor:
+                    return row[target]
+            
     # add process results to row
     async def add_result_to_today_line_row(self, line, result):
-        db = await aiosqlite.connect(self.db)
-        try:
-            await db.execute(
-                f"""Update results SET {result} WHERE line_name='{line['line_name']}' and date = '{today}'"""
-                )
-            await db.commit()
-            await db.close()
-        except Exception as ex:
-            print(ex)
-    
+        async with self._aiosqlite as db:
+            await db.execute(f"""Update results SET {result} WHERE line_name='{line['line_name']}' and date = '{today}'""")
+
     def get_today_results(self, lines):
         """
         Get lines rows in a dictioneries format
         {'line_id': x, 'line_name': y}
         and return a list of the values
         """
-        conn = sqlite3.connect(self.db)
-        with conn:
+        with self._sqlite as conn:
             conn.row_factory = dict_factory
-            rows: list = [] 
-            for line in lines:
-                curs = conn.cursor()
-                curs.execute(f"SELECT * FROM results where line_name = '{line['line_name']}' and date= '{today}'")
-                row = curs.fetchone()
-                rows.append(row)
-            return rows
- 
+            return [ conn.execute(f"SELECT * FROM results where line_name = '{line['line_name']}' and date= '{today}'").fetchone() for line in lines ]
 
-    
 if __name__ == "__main__":
     pass
